78a5360c70cf115ea0c831270099a5b94b5db51e
Allen Lowe <lallenlowe@gmail.com>
2019-08-29 20:00:48 -0600

    make bus interface more like a real bus, add separate read and write passes for all registers and memory interfacing to simulate the simultaneous nature of real hardware

--- test/index.js
+++ test/index.js
@@ -4,7 +4,6 @@ const { dec2bin } = require('../lib/common');
 const machine = require('../lib/initial-state');
 const { interfaceMemory } = require('../lib/memory');
 const { setStatusFlag, getStatusFlag, interfaceRegister } = require('../lib/register');
-const { interfaceBus } = require('../lib/bus');
 
 const test = () => {
   const cpuRegisters = machine.setupCpuRegisters();
@@ -22,25 +21,23 @@ const test = () => {
   ({ bus: mainBus, memory: systemMemory } = interfaceMemory({
     bus: mainBus,
     memory: systemMemory,
-    read: false,
-    write: true,
+    output: false,
+    input: true,
   }));
 
   ({ bus: mainBus, memory: systemMemory } = interfaceMemory({
     bus: mainBus,
     memory: systemMemory,
-    read: true,
-    write: false,
+    output: true,
+    input: false,
   }));
 
   ({ bus: mainBus, register: cpuRegisters.x } = interfaceRegister({
     bus: mainBus,
     register: cpuRegisters.x,
-    read: false,
-    write: true,
+    output: false,
+    input: true,
   }));
-
-  mainBus = interfaceBus({ bus: mainBus, readAddress: false, readData: true });
 
   console.log(getStatusFlag(cpuRegisters.status, statusFlagMap, 'Z'));
   console.log(dec2bin(cpuRegisters.status));
